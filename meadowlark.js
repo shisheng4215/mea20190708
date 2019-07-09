2fc611af2a2ac5bed247235ef4432ab0be33c463
shisheng4215 <shisheng4215@163.com>
2019-07-09 22:54:23 +0800

    Moved 'fortune cookie' functionality into module.;

--- meadowlark.js
+++ meadowlark.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var fortune = require('./lib/fortune.js');
 
 var app=express();
 
@@ -28,7 +29,7 @@ app.get('/',function(req,res){
 });
 
 app.get('/about',function(req,res){
-		var randomFortune = fortunes[Math.floor(Math.random()*fortunes.length)];
+		var randomFortune = fortune.getFortune();
 		res.render('about',{fortune:randomFortune});
 		
 
@@ -52,11 +53,5 @@ app.listen(app.get('port'),function(){
 	console.log('Express started on http://localhost:'+app.get('port')+'; press Ctrl-C to termainate.');
 });
 
-var fortunes = [
-	"Conquer your fears or they will conquer you.",
-	"Rivers need springs.",
-	"Do not fear what you don't know.",
-	"You will have a pleasant surprise.",
-	"Whenever possible,keep it simple."
-];
+
 
